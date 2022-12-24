38cd910b09c699aefcdec399369a5e2933d7dee1
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2022-12-24 21:23:50 +0300

    removed codewars tasks

--- tasks.js
+++ tasks.js
@@ -194,33 +194,7 @@ const user1 = {
   
   // Sample usage - do not modify
   console.log(getFullNames(people)); // [Sam Blue, Charlie Bon]
-  
-  /*CODEWARS */
-  function scramble(str1, str2) {
-    for (const char2 of str2) {
-      if (!str1.includes(char2)) {
-        return false;
-      }
-    }
-    return true;
-  }
-  
-  console.log(scramble("rkqodlw", "world")); // true
-  console.log(scramble("cedewaraaossoqqyt", "codewars")); // true
-  console.log(scramble("katas", "steak")); // false
-  
-  /*CODEWARS */
-  function domainName(url) {
-    url = url.replace("https://", "");
-    url = url.replace("http://", "");
-    url = url.replace("www.", "");
-  
-    return url.split(".")[0];
-  }
-  
-  console.log(domainName("http://google.com"));
-  console.log(domainName("http://google.co.jp"));
-  console.log(domainName("http://youtube.com"));
+
   
   /*task. Разделение объектов */
   /**

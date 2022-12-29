69e5b5ed4eb5843329b83f2b3b90f99afb10445a
Daria <118595398+dariavarlamovaa@users.noreply.github.com>
2022-12-29 17:56:46 +0300

    added a new task
    Implement instance methods

---
+++ task_methods/js/index.js
@@ -0,0 +1,12 @@
+/* index.js */
+import User from "./user.js";
+
+
+// class usage
+let sam = new User("Sam", "Blue");
+console.log(sam.getFullName());
+
+let charlie = new User("Charlie", "Doe");
+console.log(charlie.getFullName());
+
+console.log("---");
